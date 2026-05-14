ab221e454d98cd377259ea425bab05e1a1ab68e7
LIM QIAO ERN <limqiaoern.qe@gmail.com>
2026-05-14 22:09:08 +0800

    Updated inventory

--- inventory.py
+++ inventory.py
@@ -1,3 +1,13 @@
+import pygame
+
+pygame.init()
+
+screen_width = 800
+screen_height = 600
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Inventory")
+
 # Inventory System
 class Inventory:
     def __init__(self):
