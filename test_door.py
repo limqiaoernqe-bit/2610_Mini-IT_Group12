5285b31da19b0f3c22063e4811f3d9e7113222b9
LIM QIAO ERN <limqiaoern.qe@gmail.com>
2026-05-28 22:40:20 +0800

    Updated test_door.py

--- test_door.py
+++ test_door.py
@@ -1,11 +1,16 @@
-from inventory import Inventory, JANITOR_KEY
+from inventory import Inventory, JANITOR_KEY, ROOM_210_KEY
 from door import Door
 
-# Create inventory and door
+# Create inventory
 inventory = Inventory()
-janitor_room = Door("Janitor Room", JANITOR_KEY)
 
-# Show initial door status
+# Create doors
+janitor_room = Door("Janitor Room", JANITOR_KEY)
+room_210 = Door("Room 210", ROOM_210_KEY)
+
+# Test Janitor Room
+print("=== JANITOR ROOM TEST ===")
+
 janitor_room.show_status()
 
 # Try unlocking without key
@@ -13,13 +18,36 @@ inventory.use_item(JANITOR_KEY, janitor_room)
 
 print()
 
-# Add janitor key
+# Add key
 inventory.add_item(JANITOR_KEY)
 
 print()
 
-# Try unlocking again
+# Unlock door
 inventory.use_item(JANITOR_KEY, janitor_room)
 
-# Show updated door status
+# Show status
 janitor_room.show_status()
+
+print("\n")
+
+# Test Room 210
+print("=== ROOM 210 TEST ===")
+
+room_210.show_status()
+
+# Try unlocking without key
+inventory.use_item(ROOM_210_KEY, room_210)
+
+print()
+
+# Add key
+inventory.add_item(ROOM_210_KEY)
+
+print()
+
+# Unlock door
+inventory.use_item(ROOM_210_KEY, room_210)
+
+# Show status
+room_210.show_status()
